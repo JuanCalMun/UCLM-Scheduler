71c1ba8eec161e11e288720fe3f68fb4540a3015
JuanCalMun <juancalmun@gmail.com>
2020-04-11 22:44:48 +0200

    Added custom exceptions and request by SubjectId

--- src/main/java/com/uclm/scheduler/infrastructure/rest/v1/SubjectRestController.java
+++ src/main/java/com/uclm/scheduler/infrastructure/rest/v1/SubjectRestController.java
@@ -1,9 +1,11 @@
-package com.uclm.scheduler.controller.rest.v1;
+package com.uclm.scheduler.infrastructure.rest.v1;
 
 import com.uclm.scheduler.application.subject.SubjectFetcher;
 import com.uclm.scheduler.domain.subject.SubjectDTO;
+import com.uclm.scheduler.domain.subject.SubjectDetailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +16,13 @@ public final class SubjectRestController extends SchedulerRestController {
     @Autowired
     SubjectFetcher subjectFetcher;
 
-    @GetMapping(path = "/subject")
+    @GetMapping("/subjects")
     public List<SubjectDTO> provideAllSubjects() {
         return subjectFetcher.fetchAllSubjects();
+    }
+
+    @GetMapping("/subjects/{subjectId}")
+    public SubjectDetailDTO provideSubjectDetailsBy(@PathVariable final long subjectId) {
+        return subjectFetcher.fetchSubjectDetail(subjectId);
     }
 }
